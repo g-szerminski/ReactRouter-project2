58671f8c427f5e9ede16fa028115873b5917b57e
Grzegorz Szermiński <szerman@poczta.fm>
2019-07-15 14:50:57 +0200

    work in progress

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,9 +1,15 @@
 import React from "react";
-
 import "../styles/App.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
-function App() {
-  return <div className="App">Strona SPA</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">Strona SPA</div>
+      </Router>
+    );
+  }
 }
 
 export default App;
