799a89d2a634efeee2ec69e1b39f0d53eb6d0bd2
Grzegorz Szermiński <szerman@poczta.fm>
2019-07-17 13:05:45 +0200

    add header structure, styles and images

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,6 +1,40 @@
 import React from "react";
+import "../styles/Header.css";
+// import { Route, Switch } from "react-router-dom";
+import img1 from "../images/header1.jpg";
+import img2 from "../images/header2.jpg";
+import img3 from "../images/header3.jpg";
+// import img404 from "../images/404.png";
+
 const Header = () => {
-  return <div>Header</div>;
+  const images = [img1, img2, img3];
+  const index = Math.floor(Math.random() * 3);
+  const img = images[index];
+  return (
+    <>
+      <img src={img} alt='header' />
+      {/* <Switch>
+        <Route
+          path='/'
+          exact
+          render={() => <img src={img1} alt='obrazek 1' />}
+        />
+        <Route
+          path='/products'
+          render={() => <img src={img2} alt='obrazek 2' />}
+        />
+        <Route
+          path='/contact'
+          render={() => <img src={img3} alt='obrazek 3' />}
+        />
+        <Route
+          path='/admin'
+          render={() => <img src={img1} alt='obrazek 4' />}
+        />
+        <Route render={() => <img src={img404} alt='obrazek błąd' />} />
+      </Switch> */}
+    </>
+  );
 };
 
 export default Header;
