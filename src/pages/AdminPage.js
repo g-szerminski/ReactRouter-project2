f42162318a2ddee3379ae1b9d8bf948d0a2c316c
Grzegorz Szermiński <szerman@poczta.fm>
2019-07-21 15:19:38 +0200

    add Redirect component in admin tab

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const permition = true;
 
 const AdminPage = () => {
-  return <div>Admin</div>;
+  return (
+    <Route
+      render={() =>
+        permition ? <h3>Panel admina</h3> : <Redirect to="/login" />
+      }
+    />
+  );
 };
 
 export default AdminPage;
