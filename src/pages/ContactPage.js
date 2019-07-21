5c7436147a2663edae9403ebcaa3ce10731f494b
Grzegorz Szermiński <szerman@poczta.fm>
2019-07-21 15:02:59 +0200

    add logic of contact with Prompt component

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,41 @@
 import React from "react";
+import "../styles/ContactPage.css";
+import { Prompt } from "react-router-dom";
 
-const ContactPage = () => {
-  return <div>Kontakt</div>;
-};
+class ContactPage extends React.Component {
+  state = {
+    value: ""
+  };
+
+  handleChange = e => {
+    this.setState({
+      value: e.target.vlaue
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      value: ""
+    });
+  };
+
+  render() {
+    return (
+      <div className="contact">
+        <form onSubmit={this.handleSubmit}>
+          <h3>Napisz do nas</h3>
+          <textarea
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="...wpisz wiadomość"
+          />
+          <button>Wyślij</button>
+        </form>
+        <Prompt when={this.state.value} message='Masz niewypełniony formularz. Czy napewno chcesz opuścić stronę'/>
+      </div>
+    );
+  }
+}
 
 export default ContactPage;
